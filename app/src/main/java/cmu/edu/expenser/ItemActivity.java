f073e31e9041fa66d52010f23c0b9083376a142e
sliu2200899 <sliu2200899@gmail.com>
2017-07-31 22:00:50 -0400

    sharing

--- app/src/main/java/cmu/edu/expenser/ItemActivity.java
+++ app/src/main/java/cmu/edu/expenser/ItemActivity.java
@@ -5,10 +5,11 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,17 +17,17 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
-import static android.R.attr.id;
-import static cmu.edu.expenser.R.string.people;
-import static cmu.edu.expenser.R.string.total;
 
 public class ItemActivity extends AppCompatActivity {
 
@@ -38,6 +39,7 @@ public class ItemActivity extends AppCompatActivity {
     private static Button saveItemButton;
     private static Button deleteItemButton;
     private static String partFilename = "";
+    private static  Button splitButton;
 
     private String userId;
     private String total;
@@ -45,6 +47,8 @@ public class ItemActivity extends AppCompatActivity {
     private String category;
     private String people;
     private String photoUri;
+
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,13 +107,13 @@ public class ItemActivity extends AppCompatActivity {
         mMainImage = (ImageView) findViewById(R.id.main_image);
         if (photoUri != null && photoUri.length() != 0) {
             String storeFilename = "/photo_" + photoUri + ".jpg";
-            Bitmap bitmap = getImageFileFromSDCard(storeFilename);
+            bitmap = getImageFileFromSDCard(storeFilename);
             mMainImage.setImageBitmap(bitmap);
         }
         else {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inMutable=true;
-            Bitmap bitmap = BitmapFactory.decodeResource(
+            bitmap = BitmapFactory.decodeResource(
                     getApplicationContext().getResources(),
                     R.drawable.placeholder,
                     options);
@@ -122,6 +126,9 @@ public class ItemActivity extends AppCompatActivity {
         deleteItemButton = (Button) findViewById(R.id.deleteItemButton);
         deleteItemButton.setOnClickListener(deleteItemButtonClicked);
 
+        // splitButton
+        splitButton = (Button) findViewById(R.id.splitButton);
+        splitButton.setOnClickListener(splitButtonClicked);
     }
 
     private Bitmap getImageFileFromSDCard(String filename) {
@@ -151,6 +158,13 @@ public class ItemActivity extends AppCompatActivity {
             Context context = getApplicationContext();
             deleteItem();
             finish();
+        }
+    };
+
+    View.OnClickListener splitButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            splitExpense();
         }
     };
 
@@ -191,5 +205,54 @@ public class ItemActivity extends AppCompatActivity {
     private void deleteItem() {
         ItemDAO dbhelper = new ItemDAO(this);
         dbhelper.deleteItem(userId, total, date, category, people, photoUri);
+    }
+
+    private void splitExpense() {
+        // amount
+        String amount = totalEditText.getText().toString();
+        if (amount.trim().toString().equals("")) {
+            // TODO: alert amount is empty
+            Toast.makeText(getApplicationContext(), "Amount is empty. Please input amount",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        double amountNum = Double.parseDouble(amount);
+        if (Double.compare(amountNum, 0) == 0) {
+            // TODO: alert amount is empty
+            Toast.makeText(getApplicationContext(), "Amount is empty. Please input amount",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // number of people
+        String people = peopleEditText.getText().toString();
+        if (people.trim().toString().equals("")) {
+            // TODO: alert people is empty
+            Toast.makeText(getApplicationContext(), "Number of people is empty. Please input the number of people to share the bill.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        int peopleNum = Integer.parseInt(people);
+        if (peopleNum <= 1) {
+            // TODO: alert people is empty
+            Toast.makeText(getApplicationContext(), "Number of people should be greater than 1. Please the input number of people to share the bill.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        double dividedNum = amountNum / peopleNum;
+        Log.d("ItemActivity", "DividedNum: " + dividedNum);
+        Toast.makeText(getApplicationContext(), "Everyone should pay you $" + dividedNum + ".",
+                Toast.LENGTH_LONG).show();
+
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(bitmap)
+                .build();
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+
+        ShareDialog.show(this, content);
     }
 }
